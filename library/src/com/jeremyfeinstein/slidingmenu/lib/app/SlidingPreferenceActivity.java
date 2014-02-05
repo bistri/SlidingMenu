d01bfb1aea0409ae40257094a5f71de5c26cce48
CmoaToto <totofouques@gmail.com>
2014-02-05 18:12:54 +0100

    Change Activities by SherlockActivities in the code
    But you still need to import the ActionBarSherlock lib yourself

--- library/src/com/jeremyfeinstein/slidingmenu/lib/app/SlidingPreferenceActivity.java
+++ library/src/com/jeremyfeinstein/slidingmenu/lib/app/SlidingPreferenceActivity.java
@@ -3,12 +3,13 @@ package com.jeremyfeinstein.slidingmenu.lib.app;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 
-public class SlidingPreferenceActivity extends PreferenceActivity implements SlidingActivityBase {
+import com.actionbarsherlock.app.SherlockPreferenceActivity;
+
+public class SlidingPreferenceActivity extends SherlockPreferenceActivity implements SlidingActivityBase {
 
 	private SlidingActivityHelper mHelper;
 
